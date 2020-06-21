4d7d4fa67f741cfdc46bd20e8c6f4cf5eb0dc18a
Omenranr <omar.rachidilalaoui@um5s.net.ma>
2020-06-21 20:56:48 +0100

    modify query data

--- routes/NewsRoute.js
+++ routes/NewsRoute.js
@@ -10,11 +10,11 @@ router.get('/byCountry', (req, res, next) => {
     newsapi.v2.topHeadlines({
       q: req.query.q,
       country: req.query.country,
-      from: "2020-05-20",
+      from: "2020-06-01",
       sortBy: "publishedAt",
     }).then(response => {
       res.status(200).json(response)
-    })    
+    })   
 })
 
 module.exports = router
